964d6d9157cd568bed4437d81028dbe3bacd784d
Rushion <rushionchegge@gmail.com>
2024-07-22 13:16:33 +0300

    handled POST requests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+  const url = "http://localhost:3000/users";
+
+  const userData = {
+    name: name,
+    email: email,
+  };
+
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML += `<p>New user ID: ${data.id}</p>`;
+    })
+    .catch((error) => {
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
